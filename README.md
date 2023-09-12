648b2637507037e77fe095a20865e7532fc0a9c9
Francesco Battaglini <francesco.battaglini@endava.com>
2023-09-12 15:08:17 +1000

    script for initial setup

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-Will be populated
+# GCP infrastructure setup
+
+## Bootstrapping
+
+### Prerequisites
+
+- `gcloud` CLI
+- `jq`
+- The project ID that you want to target
+
+### Very first run
+
+We will assume that our newly-created GCP project is completely blank. That is, no APIs enabled, no service accounts available etc. etc.
+
+Before we can even run our very first github actions, there's a couple of essential steps we need to perform from our own machine to create the bare minimum that GH will need.
+
+Start by authenticating the `gcloud` CLI with your GCP account by running:
+
+```
+gcloud auth login
+```
+
+Now, let's run the `create-bootstrap-sa.sh` bash script:
+
+```
+sh ./scripts/create-bootstrap-sa.sh your-project-id-here
+```
+
+### Result
+
+The previous script will do a bunch of things:
+
+- Enable the IAM API
+- Create a new service account called `infra-bootstrap`
+- Create a new role called `infra.bootstrap`
+- Assign the `infra.bootstrap` role to the `infra-bootstrap` service account
+- Create a new private key for the SA, and download it into a `sa_key.json` file inside this directory
+
+We'll use the contents of `sa_key.json` and add them to a `GCP_CREDENTIALS` 
+
+**Warning:** careful not to leak the private key anywhere. Once you add it to the repo secrets, remove it from your local machine
---
+++ scripts/create-bootstrap-sa.sh
@@ -0,0 +1,66 @@
+# Make sure project id is passed as an argument
+if [ -z "$1" ]; then
+    echo "No project ID provided"
+    exit 1
+fi
+
+PROJECT_ID=$1
+ROLE_NAME=infra.bootstrap
+PRIVATE_KEY_FILENAME=sa_key.json
+SA_ID=infra-bootstrap
+
+# Make sure gcloud CLI is present
+if ! type gcloud >/dev/null 2>&1; then
+    echo "No gcloud CLI found on your system. Please install gcloud before running this script."
+    exit 1
+fi
+
+# Make sure a user is logged into the gcloud CLI
+AVAILABLE_ACCOUNTS=$(
+    gcloud auth list --format="json" | jq length
+)
+if [ "$AVAILABLE_ACCOUNTS" -eq "0" ]; then
+    echo "No accounts signed in with gcloud. Please sign in with gcloud auth login."
+    exit 1
+fi
+
+# Check if the project ID is available
+PROJECT_INDEX=$(
+    gcloud projects list --format="json" | jq '.[] | [ .projectId ]' | jq -s 'add | index( "${PROJECT_ID}" )'
+)
+if [ -n "$PROJECT_INDEX" ]; then
+    echo "No project with ID $1 was found. Please check the project ID again."
+    exit 1
+fi
+
+# Enable IAM APIs
+gcloud services enable iam.googleapis.com \
+    --project="${PROJECT_ID}"
+
+# Create new SA used for bootstrapping
+gcloud iam service-accounts create ${SA_ID} \
+    --description="SA used for bootstrapping GCP infrastructure for a new project" \
+    --project="${PROJECT_ID}"
+
+SA_EMAIL=${SA_ID}@${PROJECT_ID}.iam.gserviceaccount.com
+
+# Create a role with the necessary permissions for bootstrapping
+gcloud iam roles create ${ROLE_NAME} \
+    --project="${PROJECT_ID}" \
+    --file="./bootstrap-sa-role.yaml"
+
+# Get the ID of the role we just created
+ROLE_ID=$(
+    gcloud iam roles describe ${ROLE_NAME} \
+        --project="${PROJECT_ID}" \
+        --format="value(name)"
+)
+
+# Bind the role to the SA
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SA_EMAIL}" \
+    --role="${ROLE_ID}"
+
+# Create and download a SA key
+gcloud iam service-accounts keys create ./${PRIVATE_KEY_FILENAME} \
+    --iam-account="${SA_EMAIL}"
